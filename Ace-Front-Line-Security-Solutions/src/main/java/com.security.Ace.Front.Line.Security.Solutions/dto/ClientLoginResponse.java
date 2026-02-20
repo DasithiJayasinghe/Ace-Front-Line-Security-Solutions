eb2fd79bec299704010cf0d5dd48c7eaa69a882f
sh1shi-q <sahanmieperera05@gmail.com>
2026-02-20 09:30:19 +0530

    feat: Add client login dto files

--- Ace-Front-Line-Security-Solutions/src/main/java/com.security.Ace.Front.Line.Security.Solutions/dto/ClientLoginResponse.java
+++ Ace-Front-Line-Security-Solutions/src/main/java/com.security.Ace.Front.Line.Security.Solutions/dto/ClientLoginResponse.java
@@ -1,13 +1,16 @@
 package com.security.Ace.Front.Line.Security.Solutions.dto;
 
-import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class LoginResponse {
+public class ClientLoginResponse {
     private String message;
     private String role;
     private String redirectUrl;
     private String token;
+    private Integer clientId;
+    private String companyName;
+    private Boolean firstLogin;
 }
