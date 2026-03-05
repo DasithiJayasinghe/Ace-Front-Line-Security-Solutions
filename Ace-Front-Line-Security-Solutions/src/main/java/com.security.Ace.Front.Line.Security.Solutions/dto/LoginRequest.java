f2d3fbf81a24de5e9e4dc201f0c7931340562712
Ruhansi  De Silva <Ruhansisasithma@gmail.com>
2026-03-05 19:45:52 +0530

    Login request and response

--- Ace-Front-Line-Security-Solutions/src/main/java/com.security.Ace.Front.Line.Security.Solutions/dto/LoginRequest.java
+++ Ace-Front-Line-Security-Solutions/src/main/java/com.security.Ace.Front.Line.Security.Solutions/dto/LoginRequest.java
@@ -1,9 +1,14 @@
 package com.security.Ace.Front.Line.Security.Solutions.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
-    private String email;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
