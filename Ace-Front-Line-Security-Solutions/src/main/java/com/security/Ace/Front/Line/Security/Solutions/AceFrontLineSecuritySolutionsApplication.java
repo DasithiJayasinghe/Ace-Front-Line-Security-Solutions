c544e9e0c580b9c0d4efae426590f6d66f757fc7
PamudiManjarie <pamudimanjarie@gmail.com>
2026-04-01 21:11:46 +0530

    Update AceFrontLineSecurityApplication.java

---
+++ Ace-Front-Line-Security-Solutions/src/main/java/com/security/Ace/Front/Line/Security/Solutions/AceFrontLineSecuritySolutionsApplication.java
@@ -0,0 +1,16 @@
+package com.security.Ace.Front.Line.Security.Solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class AceFrontLineSecuritySolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceFrontLineSecuritySolutionsApplication.class, args);
+	}
+
+}
