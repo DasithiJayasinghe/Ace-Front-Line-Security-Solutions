89ed2e7fed7dddaf9a72b0904ff1975303223d29
PamudiManjarie <pamudimanjarie@gmail.com>
2026-04-01 21:13:27 +0530

    Update AuthController.java

---
+++ Ace-Front-Line-Security-Solutions/src/main/java/com/security/Ace/Front/Line/Security/Solutions/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.security.Ace.Front.Line.Security.Solutions.controller;
+
+import com.security.Ace.Front.Line.Security.Solutions.dto.LoginRequest;
+import com.security.Ace.Front.Line.Security.Solutions.dto.LoginResponse;
+import com.security.Ace.Front.Line.Security.Solutions.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*") // Allow requests from frontend
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            LoginResponse response = authService.login(loginRequest);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        }
+    }
+}
